0081ac44fb78f709aa6000c12f2a692a0e2783a5
nefi.urena <nurenas@ucenfotec.ac.cr>
2024-07-10 03:12:15 -0600

    VBS22-82: Set up for test configuration.

--- demo/src/main/java/com/project/demo/logic/entity/user/User.java
+++ demo/src/main/java/com/project/demo/logic/entity/user/User.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-@Table(name = "user")
+@Table(name = "\"user\"")
 @Entity
 public class User implements UserDetails {
     @Id
