88d281523584e24652cd65e84188c8bdc7df65bc
Kendall Duran <119521302+kendalljdv@users.noreply.github.com>
2024-07-23 09:47:56 -0600

    Try and catch at the service

--- demo/src/main/java/com/project/demo/logic/request/ActivitiesYelpRequestService.java
+++ demo/src/main/java/com/project/demo/logic/request/ActivitiesYelpRequestService.java
@@ -2,17 +2,12 @@ package com.project.demo.logic.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class ActivitiesYelpRequestService {
@@ -23,25 +18,34 @@ public class ActivitiesYelpRequestService {
     private RestTemplate restTemplate;
 
     public String searchActivities(Float latitude, Float longitude) {
-        String baseUrl = "https://api.yelp.com/v3/businesses/search?term=delis";
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                .queryParam("latitude", latitude)
-                .queryParam("longitude", longitude);
+        try{
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(yelpApiKey);
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
+            String baseUrl = "https://api.yelp.com/v3/businesses/search?term=delis";
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                builder.toUriString(),
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                    .queryParam("latitude", latitude)
+                    .queryParam("longitude", longitude);
 
-        return response.getBody();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(yelpApiKey);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    builder.toUriString(),
+                    HttpMethod.GET,
+                    entity,
+                    String.class
+            );
+
+            return response.getBody();
+        }
+        catch (Exception e) {
+            return "There is no response from the API service";
+        }
+
 
     }
 
