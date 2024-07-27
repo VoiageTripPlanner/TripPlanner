e1f99133553071b95ad1087d1b076361fe803a27
mur-tz <131324606+mur-tz@users.noreply.github.com>
2024-07-27 00:14:25 -0600

    OpenAI Service Exception Handling
    - Global Exception Handling additions
    - Custom Exception Handling for OpenAI Service
    - Created 3 new throwable exceptions

--- demo/src/test/java/com/project/demo/service/OpenAIServiceIntegrationTests.java
+++ demo/src/test/java/com/project/demo/service/OpenAIServiceIntegrationTests.java
@@ -12,36 +12,36 @@ import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
-//@ContextConfiguration(classes = {OpenAIServiceTestConfig.class})
+@SpringBootTest
+@ContextConfiguration(classes = {OpenAIServiceTestConfig.class})
 public class OpenAIServiceIntegrationTests {
 
-//    @Autowired
-//    private OpenAIService openAIService;
-//
-//    @Value("${openai.api.key}")
-//    private String openAIKey;
-//
-//    @Value("${openai.api.url}")
-//    private String openAIUrl;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        // No need to initialize beans manually, Spring will inject them
-//    }
-//
-//    @Test
-//    public void testGenerateTravelSuggestions() throws JsonProcessingException {
-//        String query = "Location: Santa Teresa, Guanacaste, Costa Rica\nDate: December 2024";
-//        String prompt = "You are an expert in tourism worldwide. Your role is to receive as input a given location and date. You must generate suggestions about clothes, security, weather, cultural, suggested supplies and other conditions to be considered for a person that is going to travel to that place in the given date.\n\nYour only must generate a JSON output and it must have the following keys: clothing, cultural, supply, security and other. Each one of these keys must have a list containing Strings of the recommendations. Make sure the suggestions are concise and brief. This must be displayed on my application and fill a certain card of suggestions, so make sure to write it straight to the point.";
-//
-//        String actualResponse = openAIService.generateTravelSuggestions(query, prompt);
-//
-//        assertThat(actualResponse).isNotNull();
-//        assertThat(actualResponse).contains("clothing");
-//        assertThat(actualResponse).contains("cultural");
-//        assertThat(actualResponse).contains("supply");
-//        assertThat(actualResponse).contains("security");
-//        assertThat(actualResponse).contains("other");
-//    }
+    @Autowired
+    private OpenAIService openAIService;
+
+    @Value("${openai.api.key}")
+    private String openAIKey;
+
+    @Value("${openai.api.url}")
+    private String openAIUrl;
+
+    @BeforeEach
+    public void setUp() {
+        // No need to initialize beans manually, Spring will inject them
+    }
+
+    @Test
+    public void testGenerateTravelSuggestions() throws JsonProcessingException {
+        String query = "Location: Santa Teresa, Guanacaste, Costa Rica\nDate: December 2024";
+        String prompt = "You are an expert in tourism worldwide. Your role is to receive as input a given location and date. You must generate suggestions about clothes, security, weather, cultural, suggested supplies and other conditions to be considered for a person that is going to travel to that place in the given date.\n\nYour only must generate a JSON output and it must have the following keys: clothing, cultural, supply, security and other. Each one of these keys must have a list containing Strings of the recommendations. Make sure the suggestions are concise and brief. This must be displayed on my application and fill a certain card of suggestions, so make sure to write it straight to the point.";
+
+        String actualResponse = openAIService.generateTravelSuggestions(query, prompt);
+
+        assertThat(actualResponse).isNotNull();
+        assertThat(actualResponse).contains("clothing");
+        assertThat(actualResponse).contains("cultural");
+        assertThat(actualResponse).contains("supply");
+        assertThat(actualResponse).contains("security");
+        assertThat(actualResponse).contains("other");
+    }
 }
